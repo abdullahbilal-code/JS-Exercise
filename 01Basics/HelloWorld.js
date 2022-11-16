f46993010aad037c93f6c5b7ca3e7c5116afbe0d
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-16 23:31:24 +0500

    code commit

---
+++ 01Basics/HelloWorld.js
@@ -0,0 +1,10 @@
+let Name = 'Bajwa'
+let lastName = 'Usman'
+let score = 200
+let bonus = 100
+let totalScore = score + bonus
+
+
+
+
+
---
+++ 01Basics/variables.js
@@ -0,0 +1,12 @@
+var score = 100
+
+let bonus = 130
+
+const finalScore = (score + bonus) * 0.0001
+
+// console.log(finalScore)
+
+let farnheit = 100
+
+let celsius = ((farnheit - 32) * 5 / 9)
+console.log(celsius)
