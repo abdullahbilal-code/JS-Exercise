7309b615ead37a3e63ac480bf5d3f3ea4f7cac1d
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-17 01:15:42 +0500

    Marks n Grade

--- 01Basics/learnifElse.js
+++ 01Basics/learnifElse.js
@@ -8,17 +8,42 @@
 // else {
 //     console.log('I am not inside of If Block')
 // }
-var whoIsHere = 'abc'
+// var whoIsHere = 'abc'
 
-if (whoIsHere == 'user') {
-    console.log('Greeting message for users')
-    console.log('Allow access to view all courses')
+// if (whoIsHere == 'user') {
+//     console.log('Greeting message for users')
+//     console.log('Allow access to view all courses')
+// }
+// else if (whoIsHere == 'teacher') {
+//     console.log('Greeting message for TEACHER')
+//     console.log('Allow access to his course')
+// }
+// else {
+//     console.log('MESSAGE: Please verify your email')
+//     console.log('Send User Email Verification')
+// }
+
+//  Grade Problem
+//  Students Marks 
+//  10- Amazing
+//  5- Good 
+//  3- poor 
+//  0- fail  
+
+var marks = 100
+
+if (marks == 10) {
+    console.log('Amazing..!')
 }
-else if (whoIsHere == 'teacher') {
-    console.log('Greeting message for TEACHER')
-    console.log('Allow access to his course')
+else if (marks < 10 && marks >= 5) {
+    console.log('Good...!')
+}
+else if (marks < 5 && marks >= 3) {
+    console.log('Poor..!')
+}
+else if (marks = 0) {
+    console.log('FAIL..!')
 }
 else {
-    console.log('MESSAGE: Please verify your email')
-    console.log('Send User Email Verification')
+    console.log('Out of Scope')
 }
