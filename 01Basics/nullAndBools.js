80eea0969ddc66286058ece773ee1d4255b62af0
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-16 23:36:06 +0500

    u7fwt8etf8w

---
+++ 01Basics/nullAndBools.js
@@ -0,0 +1,16 @@
+//Grade system 
+
+//Grade System 
+
+//10 - Great Job
+//5- work harder
+//2- poor
+//0 - fail
+
+//Boolean - true/false
+
+let actualMarks = 7
+
+let myGrade = (actualMarks <= 10)
+console.log(myGrade)
+
