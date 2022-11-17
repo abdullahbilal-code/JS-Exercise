6ebe5cd9311e2db7fbdd492cdbe4c8f799f531dc
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-18 02:49:31 +0500

    Array concept

---
+++ 01Basics/marvels.js
@@ -0,0 +1,11 @@
+//datatype - Array 
+
+
+
+const superHeros = ['Iron man ', 'Spider Man', 'BatsMan', 'Captain America']
+
+console.log(superHeros)
+console.log(superHeros[0])
+console.log(superHeros[1])
+console.log(superHeros[superHeros.length - 1]);
+console.log(`We have ${superHeros[superHeros.length - 2]} as Super Hero`);
