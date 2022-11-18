99339b7b0c33cfa091ee77c75186dc95b2d23601
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-19 01:00:54 +0500

    Function in JS

---
+++ 01Basics/funtions.js
@@ -0,0 +1,21 @@
+
+let sayHello = function (name) {
+    console.log(`Greeting message for ${name}`)
+    console.log(`Hey ${name}`)
+}
+
+// sayHello('KAPI')
+
+let fulNameMaker = function (fname, lname) {
+    console.log('Welcome to LCO');
+    console.log(`Happy to have you ${fname} ${lname}`)
+}
+fulNameMaker('Naveed', 'Qadir')
+
+let myAdder = function (num1, num2) {
+    let added = num1 + num2
+    return added
+}
+
+let result = myAdder(3, 5)
+console.log('Your Marks out 0f 10 is ' + result); 
