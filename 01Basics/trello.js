90fe704757c3ea3aa233486dbe7d86e515930af6
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-24 23:35:34 +0500

    css file for future use

---
+++ 01Basics/trello.js
@@ -0,0 +1,36 @@
+// const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']
+
+//Start loop from 1st Index
+// for (let index = 0; index <= days.length - 1; index++) {
+//     console.log(days[index]);
+// }
+
+//Start from end of Array Index
+// for (let index = days.length - 1; index >= 0; index--) {
+//     console.log(days[index]);
+// }
+
+//const myTodos = []
+
+// myTodos.push('Buy Bread')
+// myTodos.push('Record Videos for Youtube')
+// myTodos.push('Go to Gym')
+
+// myTodos.forEach(function (todo, index) {
+//     console.log(`Your task no. ${index} is : ${todo}`);
+
+// })
+
+// myTodos.unshift('Read Ad Hoc Testing')
+// myTodos.unshift('Read Respo Testing')
+// myTodos.unshift('Read Ad Hoc Testing')
+
+// for (let index = 0; index < myTodos.length; index++) {
+//     const element = myTodos[index];
+//     console.log(element);
+
+// }
+
+// const input = windows.prompt("What's your name ?");
+const input = prompt("What's your name?");
+alert(`Your name is ${input}`);
