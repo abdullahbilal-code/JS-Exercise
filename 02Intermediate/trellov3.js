5043b0b93777d373c921868c4d5beb127110d96c
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2022-11-29 23:16:44 +0500

    This and Methods Use in JS

--- 02Intermediate/trellov3.js
+++ 02Intermediate/trellov3.js
@@ -30,11 +30,24 @@ let myTodos = {
     addTasks: function (num) {
         this.tasks = this.tasks + num;
     },
+
+    tasksDone: function (num) {
+        this.taskDone = this.taskDone - num
+    },
+
     summary: function () {
-        return `You have ${this.tasks} tasks Today !`
+        tasksleft = this.tasks + this.taskDone
+        return `You have ${tasksleft} tasks Today !`
+    },
+    resetDay: function () {
+        tasks = 0
+        taskDone = 0
     }
+
 
 }
 
-myTodos.addTasks(6)
+myTodos.addTasks(10)
+myTodos.tasksDone(3)
+
 console.log(myTodos.summary())
