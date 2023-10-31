f33e744c47a48787e381c62a55f0f9960faaf4f8
abdullahbilal-code <abdullahbilal8349@gmail.com>
2023-10-31 19:41:30 +0500

    JS beginner

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('My code space');
+console.log('Node is run time environment for JS'); 
+
+let firstname='Abdullah'; //string
+let lastname='Bilal';
+let age =25;               //number
+let isVirgin =true;        //boolean literal
+console.log(firstname);
+
+const dollarRate =230
+//dollarRate='240'
+console.log(dollarRate);
+
+let person= { 
+    name:'Abdullah', 
+    age: 25
+}
+console.log(person)
+
+//Dot Notation
+person.name= 'Bilal'
+console.log(person.name)
+
+//Bracket Notation
+person['name']='Abdullah Bilal'
+console.log(person);
